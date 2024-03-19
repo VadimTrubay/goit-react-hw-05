6ea476e57161cc475c3da092356c9029f99b8534
VadimTrubay <vadnetvadnet@ukr.net>
2024-03-19 12:28:07 +0200

    ref movie card

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,21 @@
 import { NavLink, useLocation } from "react-router-dom";
+import css from './MovieList.module.css';
 
 const MovieList = ({ movies }) => {
+  console.log('movies: ', movies);
   const location = useLocation();
 
   return (
     <div>
       <h1>Trending today</h1>
-      <ul>
-        {movies.map(({id, title, name}) => (
-          <li key={id}>
-            <NavLink to={`/movies/${id}`} state={{from: location}}>{title ? title : name}</NavLink>
+      <ul className={css.movie_list}>
+        {movies.map(({id, title, release_date, poster_path}) => (
+          <li className={css.movie_list_item} key={id}>
+              <NavLink to={`/movies/${id}`} state={{ from: location }}>
+              <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={poster_path} />
+              <h3 className={css.movie_list_title}>{title}</h3>
+              <p className={css.movie_list_release}>{release_date}</p>
+            </NavLink>
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
-import { useEffect, useState } from 'react';
-import  { getMovies }  from '../services/movieApi';
-import MovieList from '../components/MovieList/MovieList';
+import { useEffect, useState } from "react";
+import { getMovies } from "../services/movieApi";
+import MovieList from "../components/MovieList/MovieList";
+import Loader from "../components/Loader/Loader";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
-   
-    getMovies().then(data => {
+    setLoad(true);
+    getMovies().then((data) => {
       setMovies(data.data.results);
+      setLoad(false);
     });
   }, []);
-  
+
   return (
-    <MovieList  movies={movies} />
+    <>
+      {load && <Loader />}
+      <MovieList movies={movies} />
+    </>
   );
 };
 
